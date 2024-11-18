e78e3a4f262f3f460b824df5021c8954f72f8264
Sudeepa-R <sudeepar084@gmail.com>
2024-11-18 20:56:42 +0530

    authentication adding

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel:Model<User>,
+        private jwtSrvice:JwtService
+    ){}
+
+    async signUp(signUpDto){
+        const {name,email,password}=signUpDto
+        const hashedPassword=await bcrypt.hash(password,10)
+        const user=await this.userModel.create({
+            name,email,password:hashedPassword
+        })
+        
+    }
+}
